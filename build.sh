dcbb346c4fee7e798992d86508ac02db1729bd37
Maekawa <games@daemon-systems.org>
2017-11-07 21:26:44 +0900

    Update version.

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 RESOURCE_PACK_NAME=TransparentGlass
 
 RESOURCE_PACK_FILE_JAVA=${RESOURCE_PACK_NAME}.zip
-RESOURCE_PACK_FILE_BEDROCK=${RESOURCE_PACK_NAME}BE.zip
+RESOURCE_PACK_FILE_BEDROCK_WIN10=${RESOURCE_PACK_NAME}BE.zip
+RESOURCE_PACK_FILE_BEDROCK_PORTABLE=${RESOURCE_PACK_NAME}.mcpack
 
 TMPDIR=./tmp
 
@@ -21,12 +22,23 @@ cp -R ${RESOURCE_PACK_NAME}/pack.png    ${TMPDIR}/
 rm -f -r ${TMPDIR}
 
 #
-# for Bedrock Edition
+# for Bedrock Edition (Windows10)
 #
 rm -f -r ${TMPDIR}
 mkdir -p ${TMPDIR}
 cp -R ${RESOURCE_PACK_NAME}/assets/minecraft/textures ${TMPDIR}/
 cp -R ${RESOURCE_PACK_NAME}/pack_manifest.json        ${TMPDIR}/
 cp -R ${RESOURCE_PACK_NAME}/pack.png                  ${TMPDIR}/pack_icon.png
-(cd ${TMPDIR}/ && zip -r ../${RESOURCE_PACK_FILE_BEDROCK} *)
+(cd ${TMPDIR}/ && zip -r ../${RESOURCE_PACK_FILE_BEDROCK_WIN10} *)
+rm -f -r ${TMPDIR}
+
+#
+# for Bedrock Edition (iOS)
+#
+rm -f -r ${TMPDIR}
+mkdir -p ${TMPDIR}
+cp -R ${RESOURCE_PACK_NAME}/assets/minecraft/textures ${TMPDIR}/
+cp -R ${RESOURCE_PACK_NAME}/manifest.json             ${TMPDIR}/
+cp -R ${RESOURCE_PACK_NAME}/pack.png                  ${TMPDIR}/pack_icon.png
+(cd ${TMPDIR}/ && zip -r ../${RESOURCE_PACK_FILE_BEDROCK_PORTABLE} *)
 rm -f -r ${TMPDIR}
