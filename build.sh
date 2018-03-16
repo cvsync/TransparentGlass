a9c2aa11a24927c8aec746020f886ee7ceab6134
MAEKAWA Masahide <maekawa@daemon-systems.org>
2018-03-16 18:28:32 +0900

    UUID is stored by shell script.

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 VER_MAJOR=1
 VER_MINOR=2
+UUID1=84efce45-6968-4945-92f5-864af6aee9e2
+UUID2=32b5da4e-b95b-450c-a287-83f8f2db7930
 
 RESOURCE_PACK_NAME=TransparentGlass
 
@@ -22,6 +24,8 @@ cp -R ${RESOURCE_PACK_NAME}/pack.png ${TMPDIR}/
 cat ${RESOURCE_PACK_NAME}/pack.mcmeta | \
 sed "s/XXXMAJORXXX/${VER_MAJOR}/g" | \
 sed "s/XXXMINORXXX/${VER_MINOR}/g" | \
+sed "s/XXXUUID1XXX/${UUID1}/g" | \
+sed "s/XXXUUID2XXX/${UUID2}/g" | \
 cat > ${TMPDIR}/pack.mcmeta
 (cd ${TMPDIR}/ && zip -r ../${RESOURCE_PACK_FILE_JAVA} *)
 rm -f -r ${TMPDIR}
@@ -37,6 +41,8 @@ cp -R ${RESOURCE_PACK_NAME}/pack.png                  ${TMPDIR}/pack_icon.png
 cat ${RESOURCE_PACK_NAME}/pack_manifest.json | \
 sed "s/XXXMAJORXXX/${VER_MAJOR}/g" | \
 sed "s/XXXMINORXXX/${VER_MINOR}/g" | \
+sed "s/XXXUUID1XXX/${UUID1}/g" | \
+sed "s/XXXUUID2XXX/${UUID2}/g" | \
 cat > ${TMPDIR}/pack_manifest.json
 (cd ${TMPDIR}/ && zip -r ../${RESOURCE_PACK_FILE_BEDROCK_WIN10} *)
 rm -f -r ${TMPDIR}
@@ -53,6 +59,8 @@ cp -R ${RESOURCE_PACK_NAME}/pack.png                  ${TMPDIR}/pack_icon.png
 cat ${RESOURCE_PACK_NAME}/manifest.json | \
 sed "s/XXXMAJORXXX/${VER_MAJOR}/g" | \
 sed "s/XXXMINORXXX/${VER_MINOR}/g" | \
+sed "s/XXXUUID1XXX/${UUID1}/g" | \
+sed "s/XXXUUID2XXX/${UUID2}/g" | \
 cat > ${TMPDIR}/manifest.json
 (cd ${TMPDIR}/ && zip -r ../${RESOURCE_PACK_FILE_BEDROCK_PORTABLE} *)
 rm -f -r ${TMPDIR}
