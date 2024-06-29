dac53b4e21de883916e25410aede0bb274529093
MAEKAWA Masahide <maekawa@daemon-systems.org>
2024-06-29 20:44:28 +0900

    Update build script:  - remove ^M  - revision

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 
 VER_MAJOR=1
 VER_MINOR=16
+VER_REV=0
 UUID1=84efce45-6968-4945-92f5-864af6aee9e2
 UUID2=32b5da4e-b95b-450c-a287-83f8f2db7930
 
 RESOURCE_PACK_NAME=TransparentGlass
+PACK_FORMAT=34
 
 RESOURCE_PACK_FILE_JAVA=${RESOURCE_PACK_NAME}.zip
 RESOURCE_PACK_FILE_BEDROCK_WIN10=${RESOURCE_PACK_NAME}BE.zip
@@ -18,7 +20,6 @@ TMPDIR=./tmp
 #
 TEXTURES_SRCDIR=${RESOURCE_PACK_NAME}/assets/minecraft/textures/blocks
 TEXTURES_DSTDIR=${TMPDIR}/assets/minecraft/textures/block
-PACK_FORMAT=34
 rm -f ${RESOURCE_PACK_FILE_JAVA}
 rm -f -r ${TMPDIR}
 mkdir -p ${TMPDIR} ${TMPDIR}/assets
@@ -31,13 +32,15 @@ done
 # silver -> light_gray
 cp ${TEXTURES_SRCDIR}/glass_silver.png ${TEXTURES_DSTDIR}/light_gray_stained_glass.png
 cp -R ${RESOURCE_PACK_NAME}/pack.png ${TMPDIR}/
-cat ${RESOURCE_PACK_NAME}/pack.mcmeta | \
+_destfile=pack.mcmeta
+cat ${RESOURCE_PACK_NAME}/${_destfile} | \
 sed "s/XXXPACKFORMATXXX/${PACK_FORMAT}/g" | \
 sed "s/XXXMAJORXXX/${VER_MAJOR}/g" | \
 sed "s/XXXMINORXXX/${VER_MINOR}/g" | \
+sed "s/XXXREVXXX/${VER_REV}/g" | \
 sed "s/XXXUUID1XXX/${UUID1}/g" | \
 sed "s/XXXUUID2XXX/${UUID2}/g" | \
-cat > ${TMPDIR}/pack.mcmeta
+sed "s///g" > ${TMPDIR}/${_destfile}
 (cd ${TMPDIR}/ && sudo chown -R 0:0 .)
 (cd ${TMPDIR}/ && zip -r ../${RESOURCE_PACK_FILE_JAVA} . -i "*")
 sudo rm -f -r ${TMPDIR}
@@ -50,12 +53,14 @@ rm -f -r ${TMPDIR}
 mkdir -p ${TMPDIR}
 cp -R ${RESOURCE_PACK_NAME}/assets/minecraft/textures ${TMPDIR}/
 cp -R ${RESOURCE_PACK_NAME}/pack.png                  ${TMPDIR}/pack_icon.png
-cat ${RESOURCE_PACK_NAME}/pack_manifest.json | \
+_destfile=pack_manifest.json
+cat ${RESOURCE_PACK_NAME}/${_destfile} | \
 sed "s/XXXMAJORXXX/${VER_MAJOR}/g" | \
 sed "s/XXXMINORXXX/${VER_MINOR}/g" | \
+sed "s/XXXREVXXX/${VER_REV}/g" | \
 sed "s/XXXUUID1XXX/${UUID1}/g" | \
 sed "s/XXXUUID2XXX/${UUID2}/g" | \
-cat > ${TMPDIR}/pack_manifest.json
+sed "s///g" > ${TMPDIR}/${_destfile}
 (cd ${TMPDIR}/ && sudo chown -R 0:0 .)
 (cd ${TMPDIR}/ && zip -r ../${RESOURCE_PACK_FILE_BEDROCK_WIN10} . -i "*")
 sudo rm -f -r ${TMPDIR}
@@ -69,15 +74,17 @@ mkdir -p ${TMPDIR}
 cp -R ${RESOURCE_PACK_NAME}/assets/minecraft/textures ${TMPDIR}/
 cp -R ${RESOURCE_PACK_NAME}/manifest.json             ${TMPDIR}/
 cp -R ${RESOURCE_PACK_NAME}/pack.png                  ${TMPDIR}/pack_icon.png
-cat ${RESOURCE_PACK_NAME}/manifest.json | \
+_destfile=manifest.json
+cat ${RESOURCE_PACK_NAME}/${_destfile} | \
 sed "s/XXXMAJORXXX/${VER_MAJOR}/g" | \
 sed "s/XXXMINORXXX/${VER_MINOR}/g" | \
+sed "s/XXXREVXXX/${VER_REV}/g" | \
 sed "s/XXXUUID1XXX/${UUID1}/g" | \
 sed "s/XXXUUID2XXX/${UUID2}/g" | \
-cat > ${TMPDIR}/manifest.json
+sed "s///g" > ${TMPDIR}/${_destfile}
 (cd ${TMPDIR}/ && sudo chown -R 0:0 .)
 (cd ${TMPDIR}/ && zip -r ../${RESOURCE_PACK_FILE_BEDROCK_PORTABLE} . -i "*")
 sudo rm -f -r ${TMPDIR}
 
-mkdir -p ${VER_MAJOR}.${VER_MINOR}.0
-cp ./*.mcpack ./*.zip ${VER_MAJOR}.${VER_MINOR}.0/
+mkdir -p ${VER_MAJOR}.${VER_MINOR}.${VER_REV}
+cp ./*.zip ${VER_MAJOR}.${VER_MINOR}.${VER_REV}/
